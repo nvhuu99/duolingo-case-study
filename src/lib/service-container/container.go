05af0506d73376b0d1b5282e7e6c648cb49b230c
huu.nv <nvhuu.business@gmail.com>
2024-12-28 21:18:02 +0700

    ServiceContainer package

---
+++ src/lib/service-container/container.go
@@ -0,0 +1,60 @@
+package container
+
+var (
+	svContainer *ServiceContainer
+)
+
+type binding struct {
+	singleton bool
+	closure func() any
+}
+
+type ServiceContainer struct {
+	bindings map[string]binding
+	instances map[string]any
+}
+
+func Container() *ServiceContainer {
+	if svContainer == nil {
+		svContainer = &ServiceContainer{}
+		svContainer.bindings = make(map[string]binding)
+		svContainer.instances = make(map[string]any)
+	}
+
+	return svContainer
+}
+
+func (container *ServiceContainer) Bind(name string, closure func() any) {
+	container.bindings[name] = binding{
+		singleton: false,
+		closure: closure,
+	}
+}
+
+func (container *ServiceContainer) BindSingleton(name string, closure func() any) {
+	container.bindings[name] = binding{
+		singleton: true,
+		closure: closure,
+	}
+}
+
+func Resolve(name string) any {
+	container := Container()
+
+	binding, found := container.bindings[name]
+	if !found {
+		return nil
+	}
+
+	var inst any
+	if !binding.singleton {
+		inst = binding.closure()
+	} else {
+		if _, found := container.instances[name]; !found {
+			container.instances[name] = binding.closure()
+		}
+		inst = container.instances[name]
+	}
+
+	return inst
+}
