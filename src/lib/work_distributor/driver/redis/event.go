517f16bdd7485a20e238af71ec7e005042a3bd08
huu.nv <nvhuu.business@gmail.com>
2025-05-02 18:06:29 +0700

    lib - work_distributor: add redis metric support

---
+++ src/lib/work_distributor/driver/redis/event.go
@@ -0,0 +1,15 @@
+package redis
+
+const (
+	EVT_REDIS_COMMANDS_EXEC = "evt_redis_commands_exec"
+	EVT_REDIS_LOCK_RELEASED = "evt_redis_lock_released"
+)
+
+type RedisCommandExecutedEvent struct {
+	Count int8
+}
+
+type RedisLockReleasedEvent struct {
+	WaitedTimeMs int64
+	HeldTimeMs int64
+}
