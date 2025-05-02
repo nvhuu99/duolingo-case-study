eee0b0f0bcd5dd192ca217fbb562c5681715295e
huu.nv <nvhuu.business@gmail.com>
2025-05-02 18:05:02 +0700

    docker: use docker swarm

--- README.md
+++ README.md
@@ -1,9 +1,14 @@
 # duolingo-case-study
 
-cd src
-go run infra/database/campaign_db/migrate/migrate.go up
-go run infra/database/campaign_db/seed/seed.go --campaign test_1000_usr --total 1000
+Setup docker:
+    docker build -t duolingo-service ./dockerfile.dev ..
+    docker swarm init
+    docker stack deploy --detach=true -c docker-compose.yml duolingo_case_study
 
-go run util/load_simulator/load_simulator.go --campaign test_10K_usr --num 10
+Database Migration:
+    cd src
+    go run infra/database/campaign_db/migrate/migrate.go up
+    go run infra/database/campaign_db/seed/seed.go --campaign test_1000_usr --total 1000
 
-docker compose -p duolingo_case_study up -d --build
+Load 10 request:
+    go run util/load_simulator/load_simulator.go --campaign test_10K_usr --num 10
--- src/event/event_handler/service_opt_trace/service_opt_trace.go
+++ src/event/event_handler/service_opt_trace/service_opt_trace.go
@@ -1,6 +1,8 @@
 package event_handler
 
 import (
+	"net"
+	"os"
 	"time"
 
 	ed "duolingo/event/event_data"
@@ -41,6 +43,18 @@ func (e *ServiceOperationTrace) Notified(topic string, data any) {
 }
 
 func (e *ServiceOperationTrace) handleServiceOperationBegin(data any) {
+	hostname := os.Getenv("CONTAINER_DNS_NAME")
+	instanceAddr := ""
+	if addrs, err := net.InterfaceAddrs(); err == nil {
+		for _, addr := range addrs {
+			if ipNet, ok := addr.(*net.IPNet); ok && !ipNet.IP.IsLoopback() {
+				if ipNet.IP.To4() != nil {
+					instanceAddr = ipNet.IP.To4().String()
+					break
+				}
+			}
+		}
+	}
 	evtData := data.(*ed.ServiceOperationTrace)
 	evtData.Span = &lc.TraceSpan{
 		TraceId:          evtData.ParentSpan.TraceId,
@@ -50,6 +64,8 @@ func (e *ServiceOperationTrace) handleServiceOperationBegin(data any) {
 		ServiceOperation: evtData.ServiceOpt,
 		ServiceType:      evtData.ServiceType,
 		StartTime:        time.Now(),
+		InstanceId: hostname,
+		InstanceAddress: instanceAddr,
 	}
 	e.container.BindSingleton("events.data.sv_opt_trace."+evtData.OptId, func() any {
 		return evtData
