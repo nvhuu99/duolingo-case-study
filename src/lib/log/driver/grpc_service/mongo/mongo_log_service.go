e280425e2f8455ed9331f22c95d4d2095cbe6cce
huu.nv <nvhuu.business@gmail.com>
2025-05-24 15:54:20 +0700

    lib - log: grpc mongo log service should use insert many

--- src/lib/log/driver/grpc_service/mongo/mongo_log_service.go
+++ src/lib/log/driver/grpc_service/mongo/mongo_log_service.go
@@ -4,6 +4,8 @@ import (
 	"context"
 	lw "duolingo/lib/log/writer"
 	"encoding/json"
+	"log"
+	"sync"
 	"time"
 
 	"go.mongodb.org/mongo-driver/mongo"
@@ -14,28 +16,37 @@ type MongoLogService struct {
 	Database   string
 	Collection string
 
-	ctx context.Context
+	ctx             context.Context
+	bufferCh        chan any
+	bufferSizeLimit int
+
+	mu        sync.Mutex
+	lastErr   error
 
 	mongoClient *mongo.Client
 }
 
 func NewMongoLogService(ctx context.Context, uri string, db string, coll string) (*MongoLogService, error) {
 	sv := &MongoLogService{
-		ctx:        ctx,
-		Database:   db,
-		Collection: coll,
+		ctx:             ctx,
+		Database:        db,
+		Collection:      coll,
+		bufferSizeLimit: 200,
+		bufferCh:        make(chan any, 200),
 	}
 
-	opts := options.Client()
-	opts.SetConnectTimeout(30 * time.Second)
-	opts.SetSocketTimeout(10 * time.Second)
-	opts.ApplyURI(uri)
+	opts := options.Client().
+		SetConnectTimeout(30 * time.Second).
+		SetSocketTimeout(10 * time.Second).
+		ApplyURI(uri)
 
 	client, err := mongo.Connect(ctx, opts)
 	if err != nil {
 		return nil, err
 	}
+
 	sv.mongoClient = client
+	go sv.buffering()
 
 	return sv, nil
 }
@@ -45,9 +56,68 @@ func (srv *MongoLogService) Write(line *lw.Writable) error {
 	if err := json.Unmarshal(line.Content, &data); err != nil {
 		return err
 	}
-	collection := srv.mongoClient.Database(srv.Database).Collection(srv.Collection)
-	if _, err := collection.InsertOne(srv.ctx, data); err != nil {
+	srv.bufferCh <- data
+	return srv.consumeLastErr()
+}
+
+func (srv *MongoLogService) buffering() {
+	ticker := time.NewTicker(3 * time.Second)
+	defer ticker.Stop()
+
+	batch := make([]any, 0, srv.bufferSizeLimit)
+
+	write := func(batch []any) error {
+		if len(batch) == 0 {
+			return nil
+		}
+		collection := srv.mongoClient.Database(srv.Database).Collection(srv.Collection)
+		_, err := collection.InsertMany(srv.ctx, batch)
+		if err == nil {
+			log.Printf("mongo log service: insert batch len(%v)\n", len(batch))
+		}
 		return err
 	}
-	return nil
+
+	for {
+		select {
+		case <-srv.ctx.Done():
+			err := write(batch)
+			srv.setLastErr(err)
+			return
+
+		case <-ticker.C:
+			err := write(batch)
+			srv.setLastErr(err)
+			batch = batch[:0] // clear a slice without reallocating
+
+		case line := <-srv.bufferCh:
+			batch = append(batch, line)
+			if len(batch) >= srv.bufferSizeLimit {
+				err := write(batch)
+				srv.setLastErr(err)
+				batch = batch[:0]
+			}
+		}
+	}
+}
+
+func (srv *MongoLogService) consumeLastErr() error {
+	srv.mu.Lock()
+	defer srv.mu.Unlock()
+
+	err := srv.lastErr
+	srv.lastErr = nil
+	return err
+}
+
+func (srv *MongoLogService) setLastErr(err error) {
+	if err == nil {
+		return
+	}
+	srv.mu.Lock()
+	defer srv.mu.Unlock()
+
+	if srv.lastErr == nil {
+		srv.lastErr = err
+	}
 }
